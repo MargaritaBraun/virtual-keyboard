1ed2e8d8e357b8da35f8ba20bc908492fc1a4d21
Margarita Braun <margobraun777@gmail.com>
2023-04-29 08:09:49 +0300

    init: install webpack and sass

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { pull, margoName, runMargo } from "./module";
+console.log("Hello World!");
+runMargo(margoName, pull);
+const nameI = "Margo Braun";
+
+const nameArray = ['Ffff', 'Afgf', 'Ldds']
+for (let i = 0; i < nameArray.length; i++){
+  console.log(`Привет ${nameArray[i]}`)
+}
---
+++ src/module.js
@@ -0,0 +1,8 @@
+let pull = 10;
+const margoName = "Margarita"
+const runMargo = (margoName, pull) => {
+  return console.log(`${margoName} ${pull} km`);
+};
+//runMargo(margoName, pull);
+
+export {pull, margoName, runMargo}
