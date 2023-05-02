3286c4cb82411a926b37e8ba36266b8843885a78
Margarita Braun <margobraun777@gmail.com>
2023-05-02 04:55:20 +0300

    feat: add all key

--- src/key.js
+++ src/key.js
@@ -1,22 +1,264 @@
+const small = "3rem";
+const big = "8rem";
+const small_1 = "4rem";
+const small_2 = "5.5rem";
+const big_0 = "6.5rem";
+const space = "10rem";
 export const data = [
   {
-    value: "h",
-    size: "100px"
+    value: "`",
+    size: small
   },
   {
-    value: "f",
-    size: "30px"
+    value: "1",
+    size: small
   },
   {
-    value: "j",
-    size: "50px"
+    value: "2",
+    size: small
+  },
+  {
+    value: "3",
+    size: small
+  },
+  {
+    value: "4",
+    size: small
+  },
+  {
+    value: "5",
+    size: small
+  },
+  {
+    value: "6",
+    size: small
+  },
+  {
+    value: "7",
+    size: small
+  },
+  {
+    value: "8",
+    size: small
+  },
+  {
+    value: "9",
+    size: small
+  },
+  {
+    value: "0",
+    size: small
+  },
+  {
+    value: "-",
+    size: small
+  },
+  {
+    value: "=",
+    size: small
+  },
+  {
+    value: "backspace",
+    size: big
+  },
+  {
+    value: "tab",
+    size: "small_2"
+  },
+  {
+    value: "Q",
+    size: small
+  },
+  {
+    value: "W",
+    size: small
+  },
+  {
+    value: "E",
+    size: small
+  },
+  {
+    value: "R",
+    size: small
+  },
+  {
+    value: "T",
+    size: small
+  },
+  {
+    value: "Y",
+    size: small
+  },
+  {
+    value: "U",
+    size: small
+  },
+  {
+    value: "I",
+    size: small
+  },
+  {
+    value: "O",
+    size: small
+  },
+  {
+    value: "P",
+    size: small
+  },
+  {
+    value: "[",
+    size: small
+  },
+  {
+    value: "]",
+    size: small
+  },
+  {
+    value: "\\",
+    size: small
+  },
+  {
+    value: "caps lock",
+    size: small_2
+  },
+  {
+    value: "A",
+    size: small
+  },
+  {
+    value: "S",
+    size: small
+  },
+  {
+    value: "D",
+    size: small
+  },
+  {
+    value: "F",
+    size: small
+  },
+  {
+    value: "G",
+    size: small
+  },
+  {
+    value: "H",
+    size: small
+  },
+  {
+    value: "J",
+    size: small
   },
   {
     value: "K",
-    size: "130px"
+    size: small
   },
   {
     value: "L",
-    size: "30px"
+    size: small
+  },
+  {
+    value: ";",
+    size: small
+  },
+  {
+    value: "'",
+    size: small
+  },
+  {
+    value: "enter",
+    size: small_2
+  },
+  {
+    value: "shift",
+    size: big_0
+  },
+  {
+    value: "Z",
+    size: small
+  },
+  {
+    value: "X",
+    size: small
+  },
+  {
+    value: "C",
+    size: small
+  },
+  {
+    value: "V",
+    size: small
+  },
+  {
+    value: "B",
+    size: small
+  },
+  {
+    value: "N",
+    size: small
+  },
+  {
+    value: "M",
+    size: small
+  },
+  {
+    value: "<",
+    size: small
+  },
+  {
+    value: ">",
+    size: small
+  },
+  {
+    value: "/",
+    size: small
+  },
+  {
+    value: "shift",
+    size: big_0
+  },
+  {
+    value: "ctrl",
+    size: small
+  },
+  {
+    value: "fn",
+    size: small
+  },
+  {
+    value: "win",
+    size: small
+  },
+  {
+    value: "alt",
+    size: small_1
+  },
+  {
+    value: "",
+    size: space
+  },
+  {
+    value: "alt",
+    size: small_1
+  },
+  {
+    value: "fn",
+    size: small
+  },
+  {
+    value: "ctrl",
+    size: small
+  },
+  {
+    value: "←",
+    size: small
+  },
+  {
+    value: "↓",
+    size: small
+  },
+  {
+    value: "→",
+    size: small
   }
 ];
